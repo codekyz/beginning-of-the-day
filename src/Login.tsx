e4529c6299571bbbdf35802a9340e7cf53b54783
codekyz <codekyz0111@gmail.com>
2022-03-02 20:57:50 +0900

    Update Login screen

--- src/Login.tsx
+++ src/Login.tsx
@@ -5,30 +5,48 @@ const Container = styled.section`
   display: flex;
   justify-content: center;
   align-items: center;
+  background-color: ${(props) => props.theme.bgColor};
 `;
 
 const LoginForm = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: rgba(0, 0, 0, 0.2);
+  background-color: rgba(0, 0, 0, 0.1);
   padding: 100px;
   border-radius: 10px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3);
+  @media screen and (max-width: 500px) {
+    background-color: transparent;
+    box-shadow: none;
+  }
 `;
 
 const Title = styled.h1`
-  font-size: 36px;
-  margin-bottom: 20px;
+  font-size: 42px;
+  margin-bottom: 50px;
 `;
 
 const LoginInput = styled.input`
-  border: solid 1px #aaaaaa;
-  border-radius: 3px;
+  background-color: transparent;
+  border: none;
+  border-bottom: solid 1px ${(props) => props.theme.textColor};
   padding: 10px;
-  margin-bottom: 20px;
+  margin-bottom: 50px;
+  font-size: 24px;
+  text-align: center;
+  &:focus {
+    outline: none;
+  }
 `;
 
-const LoginButton = styled.button``;
+const LoginButton = styled.button`
+  background-color: ${(props) => props.theme.accentColor};
+  color: ${(props) => props.theme.bgColor};
+  padding: 10px 80px;
+  border: none;
+  text-transform: uppercase;
+`;
 
 const Login = () => {
   return (
@@ -36,7 +54,7 @@ const Login = () => {
       <Container>
         <LoginForm>
           <Title>하루의 시작</Title>
-          <LoginInput />
+          <LoginInput placeholder="Enter your name" />
           <LoginButton>Log In</LoginButton>
         </LoginForm>
       </Container>
---
+++ src/theme.ts
@@ -0,0 +1,7 @@
+import { DefaultTheme } from "styled-components";
+
+export const defaultTheme: DefaultTheme = {
+  textColor: "#2c2c54",
+  bgColor: "#f7f1e3",
+  accentColor: "#227093",
+};
